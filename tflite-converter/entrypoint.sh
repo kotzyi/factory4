15c1c00264bb4d9797123c5b1bd049e022bd35f4
Junyeop Lee <junyeop2.lee@samsung.com>
2020-10-28 13:07:51 +0900

    modify a directory name of tflite-converter to tflite_converter

--- tflite-converter/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "CONVERTING MODEL TO TF-LITE MODEL"
-python /home/tflite-converter/scripts/tflite_converter.py
-
-echo "UPLOADING TO AZURE FILESHARE"
-python /home/tflite-converter/scripts/upload_by_fileshare.py
