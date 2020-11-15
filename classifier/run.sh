4be165209f71dde48c6cc3886ad3155c17770e49
jlee <kotzyi@gmail.com>
2020-11-15 19:39:36 +0900

    change docker image @docker compose file add pythonpath

--- classifier/run.sh
+++ classifier/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-IMAGE_DIR_PATH=/datadrive/jlee/images \
+PYTHONPATH=/home/classifier/ \
+IMAGE_DIR_PATH=/home/classifier/images \
 MODEL_NAME=efficientnetb0 \
 BATCH_SIZE=16 \
 NUM_CLASSES=2 \
 AZURE_SHARE_NAME=models \
 AZURE_MODEL_DIR_PATH=detach/models \
 AZURE_STORAGE_CONNECTION_STRING="" \
-
 docker-compose up --build
